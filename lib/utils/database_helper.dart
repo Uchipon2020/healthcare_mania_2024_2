505a62ff754db3b63e7aac4a16bcffbf921f2123
Uchiyama Satoshi <60647880+Uchipon2020@users.noreply.github.com>
2026-05-05 09:23:33 +0900

    戻しました。

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -82,7 +82,7 @@ class DatabaseHelper {
     String path = '${directory.path}models.db';
     var modelsDatabase = await openDatabase(
         path,
-        version: 4,
+        version: 3,
         onCreate: _createDb,
         onUpgrade: _upgradeDB);
     return modelsDatabase;
